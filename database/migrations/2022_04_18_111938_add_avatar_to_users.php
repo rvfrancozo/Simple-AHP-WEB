f53ec374526c0dce9161f4687173c2b5861b0dbf
Rafael Françozo <rvfrancozo@gmail.com>
2022-04-20 09:04:32 -0400

    migration avatar

--- database/migrations/2022_04_18_111938_add_avatar_to_users.php
+++ database/migrations/2022_04_18_111938_add_avatar_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar');
+            $table->string('avatar')->default('none');
         });
     }
 
