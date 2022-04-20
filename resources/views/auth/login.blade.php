311507523cf62ccef2f831021aab3b9019961273
Rafael Françozo <rvfrancozo@gmail.com>
2022-04-20 09:10:16 -0400

    migration avatar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,9 +68,9 @@
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <!--<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <!--<label class="form-check-label" for="remember">
+                                    <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>-->
                                 </div>
